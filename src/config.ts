1d8caf7b583c2e039e32a0392fdb20ad055ca34a
Thomas Clausen <31285296+ToppeRTheReal@users.noreply.github.com>
2020-10-07 16:03:57 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export let token: string = "NTg0NDgwMDc1NzQ0MDE4NDYz.XeaApQ.OyfNxXNn-bPh53NX61PdiHkRwto";
+export let token: string = "PUT TOKEN HERE";
 export let prefix: string = "!";
 export let owners: Array<string> = [];
