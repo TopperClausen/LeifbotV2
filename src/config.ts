20ae6fc7a632b691ed12cc3daa514721624b1a48
thom490h <thomasclausen@topper.mydomain>
2020-10-19 21:13:58 +0200

    test

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export let token: string = "NTg0NDgwMDc1NzQ0MDE4NDYz.XPLhfQ._BQ9HLkZ5momonoaqeVdk2ae0_4";
+export let token: string = "NTg0NDgwMDc1NzQ0MDE4NDYz.XPLhfQ.TcO4jrbVXgYidK5vDH1QZAlh5eo";
 export let prefix: string = "!";
 export let owners: Array<string> = [];
 export let welcomeRole: string = "Asian Baby";
